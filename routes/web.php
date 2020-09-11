9e6163d00c420a7e0c84adc739ebcfb890424753
teruaki niikawa <n.teruaki.2301@icloud.com>
2020-09-11 00:08:38 +0000

    製作

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('match','MatchController@top');
+Route::get('match','MatchController@top')->middleware('auth');
 Route::get('post','PostController@show');
 Route::get('mypage','MypageController@show');
 Route::get('message','MessageController@show');
 Route::get('question','QuestionController@show');
 Route::get('login','LoginController@show');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
